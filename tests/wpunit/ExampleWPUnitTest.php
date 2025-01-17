67a8f6e8d8cb69d29c3f8d835e5d0bc8ae098304
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-01-17 15:40:25 +0000

    Add wp-browser setup

---
+++ tests/wpunit/ExampleWPUnitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace NewfoldLabs\WP\Module\InstallChecker;
+
+/**
+ * @coversDefaultClass \NewfoldLabs\WP\Module\InstallChecker\Example
+ */
+class ExampleWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase {
+	
+	public function test_is_true() :void
+	{
+		
+		self::assertEquals(true, true);
+	}
+	
+}
